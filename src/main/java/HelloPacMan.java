56bcc04c64c4726f48103f5844db0afec86093ce
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2017-06-16 18:01:16 +0200

    Created Gradle Project

---
+++ src/main/java/HelloPacMan.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ManuBottax on 16/06/2017.
+ */
+public class HelloPacMan {
+    public static void main(String[] args) {
+        System.out.println("Hello World from a distributed Pacman");
+    }
+}
