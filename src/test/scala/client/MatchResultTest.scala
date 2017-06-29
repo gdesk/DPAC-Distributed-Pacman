d0a1156913994f4163ec39bc4b9c294a1e4ce74f
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2017-06-29 17:13:30 +0200

    added test on match element

---
+++ src/test/scala/client/MatchResultTest.scala
@@ -0,0 +1,24 @@
+package client
+
+import java.util.Calendar
+
+import client.gameElement.Dot
+import client.utils.Point
+import org.scalatest.FunSuite
+
+/**
+  * Created by ManuBottax on 29/06/2017.
+  */
+class MatchResultTest extends FunSuite {
+
+    test("A New Result has the current date & Time") {
+      val r: MatchResult = new MatchResult(true)
+      val date: Calendar = Calendar.getInstance()
+
+
+      assert(r.date.get(Calendar.DAY_OF_MONTH) == date.get(Calendar.DAY_OF_MONTH))
+      assert(r.date.get(Calendar.HOUR) == date.get(Calendar.HOUR))
+      assert(r.date.get(Calendar.MINUTE) == date.get(Calendar.MINUTE))
+    }
+
+}
