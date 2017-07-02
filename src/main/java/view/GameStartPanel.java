7e98fb8fd0ce35887e4aa8def865dec02ed93aef
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2017-07-02 10:51:18 +0200

    first attempt to paint playground element - need to be refactored, doesn't work

--- src/main/java/view/GameStartPanel.java
+++ src/main/java/view/GameStartPanel.java
@@ -1,5 +1,13 @@
 package view;
 
+import client.Playground;
+import client.gameElement.Block;
+import client.gameElement.Dot;
+import client.gameElement.Pill;
+import client.utils.*;
+import client.utils.Dimension;
+import client.utils.Point;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,6 +19,8 @@ import java.awt.*;
  */
 
 public class GameStartPanel extends JPanel {
+
+    private Playground selectedPlayground  = new Playground(new Dimension(50,50));
 
     public GameStartPanel(){
 
@@ -25,7 +35,29 @@ public class GameStartPanel extends JPanel {
         startGame.setBorder(null);
         startGame.addActionListener(e->{
             MainFrame mainFrame = MainFrame.getInstance();
-            mainFrame.setContentPane(new JPanel());
+            Playground p = new Playground(Dimension.apply(50,50));
+
+            p.addBlock(new Block(new Point (10.0, 2.0), 1,1));
+            p.addBlock(new Block(new Point (11.0, 2.0), 1,1));
+            p.addBlock(new Block(new Point (12.0, 2.0), 1,1));
+            p.addBlock(new Block(new Point (13.0, 2.0), 1,1));
+            p.addBlock(new Block(new Point (14.0, 2.0), 1,1));
+
+            p.addBlock(new Block(new Point (11.0, 5.0), 1,1));
+            p.addBlock(new Block(new Point (11.0, 6.0), 1,1));
+            p.addBlock(new Block(new Point (11.0, 7.0), 1,1));
+            p.addBlock(new Block(new Point (11.0, 8.0), 1,1));
+
+            p.addEatable(new Dot(new Point (20.0, 20.0)));
+            p.addEatable(new Dot(new Point (20.0, 21.0)));
+            p.addEatable(new Dot(new Point (20.0, 22.0)));
+            p.addEatable(new Pill(new Point (20.0, 23.0)));
+            p.addEatable(new Dot(new Point (20.0, 24.0)));
+            p.addEatable(new Dot(new Point (20.0, 25.0)));
+            p.addEatable(new Dot(new Point (20.0, 26.0)));
+
+
+            mainFrame.setContentPane(new PlaygroundPanel(p));
             mainFrame.repaint();
         });
 
---
+++ src/main/java/view/PlaygroundPanel.java
@@ -0,0 +1,80 @@
+package view;
+
+import client.Playground;
+import client.gameElement.GameItem;
+import client.utils.*;
+import client.utils.Point;
+import scala.Int;
+import scala.Option;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * the visual representation of the playground
+ *
+ * Created by Manuel Bottax on 01/07/2017.
+ */
+public class PlaygroundPanel extends JPanel{
+
+    private Playground playground;
+
+    public PlaygroundPanel (Playground playground){
+        this.playground = playground;
+
+        this.setLayout(new GridLayout(50,50));
+        this.setBackground(new Color(0,0,0));
+
+        //JLabel l = new JLabel();
+
+        for (Double i = 0.0; i < playground.dimension().yDimension(); i ++){
+            for (Double j = 0.0; j < playground.dimension().xDimension(); j++){
+                JLabel l = new JLabel ();
+                l.setSize(24,24);
+
+
+                Option<GameItem<Double,Double>> it = playground.getElementAtPosition(new Point (i,j));
+                if (! it.isEmpty()){
+                    switch( it.get().itemType() ) {
+                        case "block" :
+                        System.out.println("Sono un block !");
+                        ImageIcon img = new ImageIcon(Utils.getResource("/images/block/24x24/horizontalBlock.png"));
+                        System.out.println("Img size : " + img.getIconHeight() + " | " + img.getIconWidth());
+                        System.out.println("position : " + i + " | " + j);
+                        l.setIcon(img);
+                        break;
+
+                        case "dot" :
+                            System.out.println("Sono un dot !");
+                            img = new ImageIcon(Utils.getResource("/images/dot/YellowDot.png"));
+                            System.out.println("Img size : " + img.getIconHeight() + " | " + img.getIconWidth());
+                            System.out.println("position : " + i + " | " + j);
+                            l.setIcon(img);
+                            break;
+
+                        case "pill" :
+                            System.out.println("Sono una pill !");
+                            img = new ImageIcon(Utils.getResource("/images/pill/YellowPill.png"));
+                            System.out.println("Img size : " + img.getIconHeight() + " | " + img.getIconWidth());
+                            System.out.println("position : " + i + " | " + j);
+                            l.setIcon(img);
+                            break;
+
+                        //....
+
+
+                    }
+                }
+
+                this.add(l);
+            }
+        }
+
+        this.repaint();
+        this.setVisible(true);
+    }
+
+
+
+
+}
