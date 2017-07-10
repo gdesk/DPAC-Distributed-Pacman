de58be969b1abef1961ce39f88094c015a11a1fc
Margherita Pecorelli <margherita.pecorelli@studio.unibo.it>
2017-07-10 17:56:10 +0200

    resolved bug cast java list to scala list

--- src/main/java/game/GameTest.java
+++ src/main/java/game/GameTest.java
@@ -7,6 +7,7 @@ import client.model.utils.Dimension;
 import client.model.utils.PointImpl;
 import client.model.controller.UserInputController;
 import client.view.*;
+import scala.collection.mutable.ListBuffer;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -65,32 +66,33 @@ public class GameTest {
         Playground playground = PlaygroundImpl$.MODULE$.instance();
         playground.dimension_$eq(Dimension.apply(60, 30));
 
-        List<Block> blocks = new ArrayList<>();
+        ListBuffer<Block> blocks = new ListBuffer<>();
+
         for (int i = 1; i < 59; i++ ){
             //p.addBlock(new Block(new PointImpl(i, 1)));
-            blocks.add(new Block(new PointImpl(i, 1)));
+            blocks.$plus$eq(new Block(new PointImpl(i, 1)));
         }
         for (int i = 1; i < 59; i++ ){
             //p.addBlock(new Block(new PointImpl(i, 29)));
-            blocks.add(new Block(new PointImpl(i, 29)));
+            blocks.$plus$eq(new Block(new PointImpl(i, 29)));
         }
         for (int j = 1; j < 29; j++ ){
             //p.addBlock(new Block(new PointImpl(1, j)));
-            blocks.add(new Block(new PointImpl(1, j)));
+            blocks.$plus$eq(new Block(new PointImpl(1, j)));
         }
         for (int j = 1; j < 29; j++ ){
             //p.addBlock(new Block(new PointImpl(59, j)));
-            blocks.add(new Block(new PointImpl(59, j)));
+            blocks.$plus$eq(new Block(new PointImpl(59, j)));
         }
         for (int j = 5; j < 29; j = j + 5 ){
             for (int i = 3; i < 58 ; i++ ) {
                 //p.addBlock(new Block(new PointImpl(i, j)));
-                blocks.add(new Block(new PointImpl(i, j)));
+                blocks.$plus$eq(new Block(new PointImpl(i, j)));
             }
         }
-        playground.blocks_$eq((scala.collection.immutable.List<Block>) blocks);
+        playground.blocks_$eq(blocks.toList());
 
-        List<Eatable> eatables = new ArrayList<>();
+        ListBuffer<Eatable> eatables = new ListBuffer<>();
         int pillId = 0;
         int dotId = 0;
         for (int j=7; j<29; j=j+5){
@@ -98,31 +100,31 @@ public class GameTest {
                 for(int i=7; i<54; i++) {
                     if (i % 14 == 0) {
                         //p.addEatable(new VirtualPill(new PointImpl(i, j)));
-                        eatables.add(new Pill("pill" + pillId++, new PointImpl(i, j)));
+                        eatables.$plus$eq(new Pill("pill" + pillId++, new PointImpl(i, j)));
                     } else {
                         //p.addEatable(new VirtualDot(new PointImpl(i, j)));
-                        eatables.add(new Dot("dot" + dotId++, new PointImpl(i, j)));
+                        eatables.$plus$eq(new Dot("dot" + dotId++, new PointImpl(i, j)));
                     }
                 }
             }
         }
         //p.addEatable(new VirtualFruit(new PointImpl(22, 17), Fruits.CHERRY));
-        eatables.add(new Cherry("cherry" + 0, new PointImpl(22, 17)));
+        eatables.$plus$eq(new Cherry("cherry" + 0, new PointImpl(22, 17)));
         //p.addEatable(new VirtualFruit(new PointImpl(23, 17), Fruits.STRAWBERRY));
-        eatables.add(new Strawberry("strawberry" + 0, new PointImpl(23, 17)));
+        eatables.$plus$eq(new Strawberry("strawberry" + 0, new PointImpl(23, 17)));
         //p.addEatable(new VirtualFruit(new PointImpl(24, 17), Fruits.ORANGE));
-        eatables.add(new Orange("orange" + 0, new PointImpl(24, 17)));
+        eatables.$plus$eq(new Orange("orange" + 0, new PointImpl(24, 17)));
         //p.addEatable(new VirtualFruit(new PointImpl(25, 17), Fruits.APPLE));
-        eatables.add(new Apple("apple" + 0, new PointImpl(25, 17)));
+        eatables.$plus$eq(new Apple("apple" + 0, new PointImpl(25, 17)));
         //p.addEatable(new VirtualFruit(new PointImpl(26, 17), Fruits.GRAPES));
-        eatables.add(new Grapes("grapes" + 0, new PointImpl(26, 17)));
+        eatables.$plus$eq(new Grapes("grapes" + 0, new PointImpl(26, 17)));
         //p.addEatable(new VirtualFruit(new PointImpl(27, 17), Fruits.GALAXIAN_SHIP));
-        eatables.add(new GalaxianShip("galaxian ship" + 0, new PointImpl(27, 17)));
+        eatables.$plus$eq(new GalaxianShip("galaxian ship" + 0, new PointImpl(27, 17)));
         //p.addEatable(new VirtualFruit(new PointImpl(28, 17), Fruits.BELL));
-        eatables.add(new Bell("bell" + 0, new PointImpl(28, 17)));
+        eatables.$plus$eq(new Bell("bell" + 0, new PointImpl(28, 17)));
         //p.addEatable(new VirtualFruit(new PointImpl(29, 17), Fruits.KEY));
-        eatables.add(new Key("key" + 0, new PointImpl(29, 17)));
-        playground.blocks_$eq((scala.collection.immutable.List<Block>) eatables);
+        eatables.$plus$eq(new Key("key" + 0, new PointImpl(29, 17)));
+        playground.eatables_$eq(eatables.toList());
 
         return playground;
     }
