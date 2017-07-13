ee1224252127f58511b7e5c0f43f649eaa5fba30
Chiara Varini <chiara.varini@studio.unibo.it>
2017-07-13 10:41:57 +0200

    created Res.scala object for resources management

---
+++ src/main/scala/client/view/Res.scala
@@ -0,0 +1,17 @@
+package client.view
+
+/**
+  * Created by chiaravarini on 13/07/17.
+  */
+object Res {
+
+  val IMAGES_BASE_PATH = "/images/"
+  val IMAGES_EXTENSION = ".png"
+  val GIF_BASE_PATH = "/gif/"
+  val GIF_EXTENSION = ".gif"
+
+  val TITLE_IMAGE = "gameTitle"
+  val LOGIN_IMAGE = "led"
+  val HOME_IMAGE = "whiteGhosts"
+
+}
