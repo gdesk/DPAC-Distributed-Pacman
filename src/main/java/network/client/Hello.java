346af7fe1aa93a00d07495eeb7063a860f729f20
Federica <federica.pecci2@studio.unibo.it>
2017-07-18 11:32:05 +0200

    switched from socket tcp to RMI, first communication example between client and server

---
+++ src/main/java/network/client/Hello.java
@@ -0,0 +1,14 @@
+package network.client; /**
+ * Created by Federica on 16/07/17.
+ */
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface Hello extends Remote {
+
+    String sayHello() throws RemoteException;
+
+}
--- src/main/java/network/client/PeerConfiguration.java
+++
@@ -1,8 +0,0 @@
-package network.client; /**
- * Created by Federica on 12/07/17.
- */
-public interface PeerConfiguration {
-
-    void peersConfig(Peer peer);
-
-}
