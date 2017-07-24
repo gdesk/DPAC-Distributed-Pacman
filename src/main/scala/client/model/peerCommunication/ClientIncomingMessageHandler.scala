5088d427beb707c0d5221f8af7b762efe0c0e8c8
Federica <federica.pecci2@studio.unibo.it>
2017-07-24 20:14:42 +0200

    added javadoc  for P2P Communication traits

--- src/main/scala/client/model/peerCommunication/ClientIncomingMessageHandler.scala
+++ src/main/scala/client/model/peerCommunication/ClientIncomingMessageHandler.scala
@@ -5,18 +5,42 @@ import java.util.Observer
 /**
   * Created by Federica on 24/07/17.
   *
-  * class to handle different type message
-  * that a peer can send to others
+  * this trait is useful to handle two types of communication:
+  * - peer to peer (so that, a peer receives info from others)
+  * - peer to controller
   */
 trait ClientIncomingMessageHandler {
 
+  /**
+    * this method allows to register controller
+    * as model observer, so that:
+    * - model is observable
+    * - controller is observer of model
+    * @param observer
+    */
   def addObserver(observer: Observer): Unit
 
-  def setRemainingLives(character: Character): Unit
+  /**
+    * this method updates the remaining lives for this peer
+    * @param character
+    */
+  def updateRemainingLives(character: Character): Unit
 
-  def setDead(character: Character): Unit
+  /**
+    * this method updates the current state (dead or alive) for this peer
+    * @param character
+    */
+  def updateDead(character: Character): Unit
 
-  def setScore (character: Character): Unit
+  /**
+    * this method updates the current score (dead or alive) for this peer
+    * @param character
+    */
+  def updateScore (character: Character): Unit
 
-  def setPosition(character: Character): Unit
+  /**
+    * this method updates the current position (dead or alive) for this peer
+    * @param character
+    */
+  def updatePosition(character: Character): Unit
 }
--- src/main/scala/client/model/peerCommunication/ClientOutcomingMessageHandler.scala
+++ src/main/scala/client/model/peerCommunication/ClientOutcomingMessageHandler.scala
@@ -5,19 +5,45 @@ import java.util.Observer
 /**
   * Created by Federica on 24/07/17.
   *
-  * trait to handle
+  * this trait is useful to handle two types of communication:
+  * - peer to peer (so that, a peer send info to others)
+  * - peer to controller
+  *
   */
-
 
 trait ClientOutcomingMessageHandler {
 
+  /**
+    * this method allows to register controller
+    * as model observer, so that:
+    * - model is observable
+    * - controller is observer of model
+    * @param observer
+    */
+  def addObserver(observer: Observer): Unit
 
-  def addObserver(observer: Observer, arg: scala.Any): Unit
+  /**
+    * method to notify controller and other peers
+    * about the number of lives left to this peer
+    * @param observer
+    * @param arg
+    */
+  def notifyRemainingLives(observer: Observer, arg: scala.Any): Unit
 
-  def notifyRemainingLives(): Unit
+  /**
+    * method to notify controller and other peers
+    * about the current total score of this peer
+    * @param observer
+    * @param arg
+    */
+  def notifyScore(observer: Observer, arg: scala.Any): Unit
 
-  def notifyScore(): Unit
-
-  def notifyPosition(): Unit
+  /**
+    * method to notify controller and other peers
+    * about the current position of this peer
+    * @param observer
+    * @param arg
+    */
+  def notifyPosition(observer: Observer, arg: scala.Any): Unit
 
 }
