c69674842251e11fc79cf05ca89ed7b45319c390
Federica <federica.pecci2@studio.unibo.it>
2017-07-28 14:55:44 +0200

    added some code so that remote objects are properly configured(bind) and updated(rebind)

--- src/main/java/network/client/P2P/game/ServerPlayingWorkerThread.java
+++ src/main/java/network/client/P2P/game/ServerPlayingWorkerThread.java
@@ -9,7 +9,7 @@ import client.model.peerCommunication.ClientOutcomingMessageHandlerImpl;
 import client.model.utils.Point;
 import network.client.P2P.bootstrap.PeerStateRegister;
 
-import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
@@ -22,66 +22,58 @@ import java.util.Map;
  */
 public class ServerPlayingWorkerThread implements Runnable, Remote {
 
-
-
     private Match match;
     private Character character;
     private int rmiPort;
     private Registry registry;
     private ClientOutcomingMessageHandler handler;
-    private Map<String, ServerPlayingWorkerThread> objects;
     private Point<Object,Object> currentPosition;
     private int currentScore;
     private Lives currentLives;
     private Boolean isDead;
+    private Map<String, ServerPlayingWorkerThread> objects;
 
 
     public ServerPlayingWorkerThread(){}
 
     public ServerPlayingWorkerThread(int rmiPort, Registry registry){
         this.match = MatchImpl.instance();
-        //this.character = match.myCharacter();
+        //this.character = match.myCharacter(); //TODO SCOMMENTARE QUANDO PUSHO SU DEVELOP
         this.rmiPort = rmiPort;
         this.registry = registry;
         this.handler = new ClientOutcomingMessageHandlerImpl();
-        this.objects = new HashMap<>();
         this.currentPosition = character.position();
         this.currentScore = character.score();
         this.currentLives = character.lives();
         this.isDead = character.isAlive();
+        this.objects = new HashMap<>();
 
     }
 
     @Override
     public void run() {
-        PeerStateRegister stub;
-
-        objects.put("positon", new ServerPlayingWorkerThread());
-        objects.put("score", new ServerPlayingWorkerThread());
-        objects.put("remaningLives", new ServerPlayingWorkerThread());
-        objects.put("death", new ServerPlayingWorkerThread());
+        initializeObjectBinding();
 
         //per far terminare questo while basta chiamare shutdown
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 if(!character.position().equals(currentPosition)){
-                    stub = (PeerStateRegister) UnicastRemoteObject.exportObject(objects.get("positon"), this.rmiPort);
-                    registry.rebind("Position", stub);
+                    registry.rebind("currentPosition", objects.get("currentPosition"));
                     this.currentPosition = character.position();
                     handler.notifyMove(character);
                 }else if(!((Integer) character.score()).equals(currentScore)){
-                    stub = (PeerStateRegister) UnicastRemoteObject.exportObject(objects.get("score"), this.rmiPort);
-                    registry.rebind("Score", stub);
+                    //stub = (PeerStateRegister) UnicastRemoteObject.exportObject(objects.get("currentScore"), this.rmiPort);
+                    registry.rebind("currentScore", objects.get("currentScore"));
                     this.currentScore = character.score();
                     handler.notifyScore(character);
                 }else if(!character.lives().equals(currentLives)){
-                    stub = (PeerStateRegister) UnicastRemoteObject.exportObject(objects.get("remaningLives"), this.rmiPort);
-                    registry.rebind("RemaningLives", stub);
+                    //stub = (PeerStateRegister) UnicastRemoteObject.exportObject(objects.get("currentLives"), this.rmiPort);
+                    registry.rebind("currentLives", objects.get("currentLives"));
                     this.currentLives = character.lives();
                     handler.notifyRemainingLives(character);
                 }else if(!character.isAlive() == isDead){
-                    stub = (PeerStateRegister) UnicastRemoteObject.exportObject(objects.get("death"), this.rmiPort);
-                    registry.rebind("Death", stub);
+                    //stub = (PeerStateRegister) UnicastRemoteObject.exportObject(objects.get("isDead"), this.rmiPort);
+                    registry.rebind("isDead", objects.get("isDead"));
                     this.isDead = character.isAlive();
                     handler.notifyDeath(character);
                 }
@@ -89,12 +81,33 @@ public class ServerPlayingWorkerThread implements Runnable, Remote {
                 wait(1000);
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
-            } catch (AccessException e) {
-                e.printStackTrace();
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
         }
+
+    }
+
+
+    private void initializeObjectBinding(){
+        PeerStateRegister stub;
+
+        this.objects.put("currentPosition", new ServerPlayingWorkerThread());
+        this.objects.put("currentScore", new ServerPlayingWorkerThread());
+        this.objects.put("currentLives", new ServerPlayingWorkerThread());
+        this.objects.put("isDead", new ServerPlayingWorkerThread());
+
+        for(Map.Entry<String, ServerPlayingWorkerThread> pair: objects.entrySet()){
+            try {
+                stub = (PeerStateRegister) UnicastRemoteObject.exportObject(pair.getValue(), this.rmiPort);
+                registry.bind(pair.getKey(), stub);
+
+            } catch (RemoteException | AlreadyBoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 
 
