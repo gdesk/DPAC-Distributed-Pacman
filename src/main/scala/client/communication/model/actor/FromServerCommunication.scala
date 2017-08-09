5a66d326f7eee877a0addfae06007880fec9c775
DESKTOP-M6HD4PO\lucch <giulia.lucchi7@studio.unibo.it>
2017-08-09 15:28:44 +0200

    Resolved error

--- src/main/scala/client/communication/model/actor/FromServerCommunication.scala
+++ src/main/scala/client/communication/model/actor/FromServerCommunication.scala
@@ -1,7 +1,7 @@
 package client.communication.model.actor
 
 import akka.actor.UntypedAbstractActor
-import client.utils.ActorUtils
+import client.utils.{ActorUtils, ControllerObservable}
 
 import scala.util.parsing.json.JSONObject
 
@@ -11,10 +11,23 @@ import scala.util.parsing.json.JSONObject
   * @author Giulia Lucchi
   */
 class FromServerCommunication extends UntypedAbstractActor{
+  val controllerObservable = ControllerObservable
+
   override def onReceive(message: Any): Unit = message match {
-    case msg: JSONObject => {
-      val receiver = context actorSelection ActorUtils.INBOX_ACTOR
-      receiver ! msg.asInstanceOf[JSONObject]
+    case msg: JSONObject => msg.obj("object") match {
+      case "friendRequest" =>{
+        val username = msg.obj("senderRequest").asInstanceOf[String]
+        controllerObservable.gameRequest("GameRequest", username)
+      }
+      case "friendResponse" =>{
+        val response = msg.obj("responseRequest").asInstanceOf[Boolean]
+        controllerObservable.gameResponse("GameResponse", response)
+      }
+      case _ => { val receiver = context actorSelection ActorUtils.INBOX_ACTOR
+        receiver ! msg.asInstanceOf[JSONObject]
+
+      }
+
     }
   }
 }
---
+++ src/test/scala/client/communication/ActorTest.scala
@@ -0,0 +1,15 @@
+package client.communication
+
+import client.communication.model.ToClientCommunicationImpl
+
+/**
+  * Created by lucch on 09/08/2017.
+  */
+object ActorTest extends App {
+  val model = ToClientCommunicationImpl()
+
+  //registration
+  assert(model.registration("giulia","giuls","lucchigiulia@libero.it","ciao", "ciao1"), false)
+  assert(model.registration("giulia","giuls","lucchigiulia@libero.it","ciao", "ciao"), true)
+
+}
