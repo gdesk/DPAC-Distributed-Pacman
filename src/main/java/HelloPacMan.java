e19e788a76bcfa7252e0784321e58f5eaa6c8c99
Chiara Varini <chiara.varini@studio.unibo.it>
2017-08-10 01:12:11 +0200

    add set model to Controller in main

--- src/main/java/HelloPacMan.java
+++ src/main/java/HelloPacMan.java
@@ -12,7 +12,7 @@ public class HelloPacMan {
 
     public static void main(String[] args) {
         ToClientCommunicationImpl clientComm = new ToClientCommunicationImpl();
-        BaseControllerUser.instance().model_$eq(clientComm);
+        BaseControllerUser.instance().model(clientComm);
         BaseControllerMatch.instance().model_$eq(clientComm);
         
         SwingUtilities.invokeLater(new Runnable() {
