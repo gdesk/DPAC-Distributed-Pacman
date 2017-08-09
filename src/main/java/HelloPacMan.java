2b22cec51320967c20c20955ad39d3cd2ac5a3d6
Chiara Varini <chiara.varini@studio.unibo.it>
2017-08-10 01:04:02 +0200

    first login test

--- src/main/java/HelloPacMan.java
+++ src/main/java/HelloPacMan.java
@@ -1,3 +1,6 @@
+import client.communication.model.ToClientCommunicationImpl;
+import client.controller.BaseControllerMatch;
+import client.controller.BaseControllerUser;
 import client.view.MainFrame;
 
 import javax.swing.*;
@@ -6,8 +9,12 @@ import javax.swing.*;
  * Created by ManuBottax on 16/06/2017.
  */
 public class HelloPacMan {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        ToClientCommunicationImpl clientComm = new ToClientCommunicationImpl();
+        BaseControllerUser.instance().model_$eq(clientComm);
+        BaseControllerMatch.instance().model_$eq(clientComm);
+        
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
