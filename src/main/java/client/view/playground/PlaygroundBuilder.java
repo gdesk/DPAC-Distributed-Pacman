bd74ac69af7b36ff00f2cb715c3a21a515d9c33c
Chiara Varini <chiara.varini@studio.unibo.it>
2017-08-12 10:14:40 +0200

    improved code in MicroMapPanel and delete PlaygroundDynamicSettings

--- src/main/java/client/view/playground/PlaygroundBuilder.java
+++ src/main/java/client/view/playground/PlaygroundBuilder.java
@@ -3,18 +3,17 @@ package client.view.playground;
 import java.awt.*;
 
 /**
- * Created by manuBottax and chiaravarini on 05/07/17.
+ * Builder for a PlaygroundView where there are all the methods
+ * for setting the different components of a PlaygroundView
+ *
+ * Created by manuBottax and Chiara Varini on 05/07/17.
  */
 public interface PlaygroundBuilder {
 
-    PlaygroundBuilder setColumns(final int colums);
+    PlaygroundBuilder setColumns(final int columns);
     PlaygroundBuilder setRows(final int rows);
     PlaygroundBuilder setBackground(final Color backgroundColor);
     PlaygroundBuilder setBackground(final Image backgroundImage);
-   // Playground setCharacters(final List<Character> characterList);
-    //TODO implementa più avanti
-    //TODO ripensa a cosa poter mettere
-
     PlaygroundView createPlayground();
 
 }
