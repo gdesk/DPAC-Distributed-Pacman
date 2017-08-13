c580e45adb701e6ad721ea39d964cde2ed03172c
Federica <federica.pecci2@studio.unibo.it>
2017-08-13 10:34:47 +0200

    passing observable to controller in model P2PCommunication

--- src/main/scala/client/model/peerCommunication/ClientOutcomingMessageHandlerImpl.scala
+++ src/main/scala/client/model/peerCommunication/ClientOutcomingMessageHandlerImpl.scala
@@ -26,7 +26,7 @@ class ClientOutcomingMessageHandlerImpl extends Observable with ClientOutcomingM
     * every Peer has finished to be configured
     */
   def startMatch(): Unit =
-    notifyObservers("StartMatch")
+    notifyObservers(this, "StartMatch")
 
 
 
