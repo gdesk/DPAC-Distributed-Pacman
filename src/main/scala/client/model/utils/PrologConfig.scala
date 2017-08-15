549ceacf4a35c40eb27755e0faf5c76aab5ab89b
DESKTOP-M6HD4PO\lucch <giulia.lucchi7@studio.unibo.it>
2017-08-15 09:56:28 +0200

    merge completed and refactoring

--- src/main/scala/client/model/utils/PrologConfig.scala
+++ src/main/scala/client/model/utils/PrologConfig.scala
@@ -5,19 +5,19 @@ import java.io.FileInputStream
 import alice.tuprolog.{Prolog, Theory}
 
 /**
-  * Created by lucch on 03/07/2017.
+  * Add the the unique theory of prolog from file.
+  *
+  * @author Giulia Lucchi
   */
-//todo: scala doc da fare
 object PrologConfig {
-
   private val FILE_NAME = "src/main/prolog/dpac-prolog.pl"
   private val _theory: Theory =  new Theory(new FileInputStream(FILE_NAME))
 
   val ENGINE = ScalaProlog.mkPrologEngine(_theory)
 
+
   def getPrologEngine: Prolog = ScalaProlog.getPrologEngine(_theory)
-
+  
   def theory: Theory = _theory
-
 
 }
